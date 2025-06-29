7822520cac52454bb83501691fd483f2a213eba5
Arbi Stepanian <arbi.stepanian@gmail.com>
2025-06-29 00:05:10 -0700

    adding health check endpoint

--- backend/src/api/routes/__init__.py
+++ backend/src/api/routes/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from .data import router as data_router
+from .healthcheck import router as health_check_router
 
 router = APIRouter()
 router.include_router(data_router, prefix="/api/data", tags=["data"])
+router.include_router(health_check_router, prefix="/api/healthcheck", tags=["health_check"])
---
+++ backend/src/api/routes/healthcheck.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+
+router = APIRouter()
+
+@router.get("/")
+def heath_check():
+    return JSONResponse({"status": "ok"})
